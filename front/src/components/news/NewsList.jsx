98d5c0b017351ef0e88b8466e8eb9ca9e0da0954
apavlov3rednet <92976265+apavlov3rednet@users.noreply.github.com>
2024-05-25 09:24:07 +0300

    subsections

--- front/src/components/news/NewsList.jsx
+++ front/src/components/news/NewsList.jsx
@@ -15,21 +15,46 @@ export default function NewsList({collectionName, limit, paginator = false}) {
         () => {fetchNews()}, [fetchNews]
     );
 
+    function getSections() {
+        let arElements = {};
+
+        news.map(item => {
+            if(!arElements[item.CATEGORY])
+                arElements[item.CATEGORY] = [];
+
+            arElements[item.CATEGORY].push(item);
+        });
+
+        console.log(arElements);
+
+        return (
+            <>
+                {
+                    Object.keys(arElements).map(section => (
+                        <>
+                            <h2>{section}</h2>
+                            {
+                                arElements[section].map(el => (
+                                    <div className='news-card'>
+                                        <img src={'./' + el.IMAGE} alt={el.NAME} />
+                                        <h2>{el.NAME}</h2>
+                                        <span>{el.OPISANUE}</span>
+                                        {el.TROFY != '' && <span className='params'>Награда: <b>{el.TROFY}</b></span>}
+                                        {el.CLASS != '' && <span className='params'>Класс: <b>{el.CLASS}</b></span>}
+                                    </div>
+                                ))
+                            }
+                        </>
+                    ))
+                }
+            </>
+        )
+    }
+
     return (
         <>
-        <h2>Мобы</h2>
         <div className='news-list'>   
-            {
-                news && news.map(el => (
-                    <div className='news-card'>
-                        <img src={'./' + el.IMAGE} alt={el.NAME} />
-                        <h2>{el.NAME}</h2>
-                        <span>{el.OPISANUE}</span>
-                        {el.TROFY != '' && <span className='params'>Награда: <b>{el.TROFY}</b></span>}
-                        {el.CLASS != '' && <span className='params'>Класс: <b>{el.CLASS}</b></span>}
-                    </div>
-                ))
-            }
+            {getSections()}
         </div>
         </>
     )
